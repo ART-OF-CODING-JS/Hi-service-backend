53936f82e63ab9ccb1836cfb1278e136190a41b9
HebaAlhamaydh <Eng_heba_hamaydh@hotmail.com>
2022-07-21 13:52:12 +0300

    add middleware

---
+++ src/middleware/basic.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const base64 = require('base-64');
+const { users } = require('../models');
+// const bearer = require('./bearer')
+
+module.exports = async (req, res, next) => {
+
+   if (!req.headers.authorization) {
+    next()
+     return _authError(); 
+    };
+
+  let basic = req.headers.authorization.split(' ').pop();
+
+  let [username, pass] =base64.decode(basic).split(':');
+  try {
+    
+    req.user=await users.authenticateBasic(username, pass);
+    // res.cookie('jwt', req.user.token,{
+    //   httpOnly:true
+    // });
+      next();
+   
+
+  } catch (e) {
+  console.error(e);
+    res.status(403).send('Invalid Login');
+  }
+
+}
