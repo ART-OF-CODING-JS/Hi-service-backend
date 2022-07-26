d0f0d1cc6ed32026f8d6f896c933189830688cd7
Mohammad Alhaj <mohammadbasil.alhaj@gmail.com>
2022-07-26 11:55:29 +0300

    editEmail

--- src/routers/email.js
+++ src/routers/email.js
@@ -1,23 +1,29 @@
 const nodeMailer = require("nodemailer");
 
-const anyEmail = record.email;
-let transporter = nodeMailer.createTransport({
-    service: "hotmail",
-    auth: {
-        user: "hi-service-js@outlook.com", // generated ethereal user
-        pass: "hiservice123", // generated ethereal password
-    },
-});
-let info = {
-    from: 'hi-service-js@outlook.com', // sender address
-    to: anyEmail, // list of receivers
-    subject: "Hi service",
-    text: "welcome to our application", 
-};
-transporter.sendMail(info, function (err, infoo) {
-    if (err) {
-        console.log("errr : ", err);
-        return;
-    }
-    console.log("infoo : ", infoo.response);
-})
+
+function mail(email){
+
+    let transporter = nodeMailer.createTransport({
+        service: "hotmail",
+        auth: {
+            user: "hi-service-js@outlook.com", // generated ethereal user
+            pass: "hiservice123", // generated ethereal password
+        },
+    });
+    let info = {
+        from: 'hi-service-js@outlook.com', // sender address
+        to: email, // list of receivers
+        subject: "Hi service",
+        text: "welcome to our application", 
+    };
+    transporter.sendMail(info, function (err, infoo) {
+        if (err) {
+            console.log("errr : ", err);
+            return;
+        }
+        console.log("infoo : ", infoo.response);
+    })
+}
+module.exports = mail;
+
+
