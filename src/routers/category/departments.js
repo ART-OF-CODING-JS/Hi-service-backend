2ecdb9ac6e3c7fcc770fbadd7b743ca92800c8c4
HebaAlhamaydh <Eng_heba_hamaydh@hotmail.com>
2022-07-29 10:17:38 +0300

    add loger

--- src/routers/category/departments.js
+++ src/routers/category/departments.js
@@ -3,6 +3,7 @@
 const { service } = require("../../models/index-model");
 const express = require('express');
 const bearer = require("../../middleware/bearer")
+const loger = require('../../logger')
 
 const departRouter = express.Router();
 
@@ -16,9 +17,7 @@ departRouter.post('/department',bearer,async(req, res)=>{
       
       const departments = await service.findAll({where:{department:department}})
 
-      // loger.info(findUser.username+" "+"is un blocked by admin ", {
-      //   timestamp: new Date().toString(),
-      // })
+      loger.info("user"+req.user.id+" "+"search in this departement "+req.body.department+" ", {timestamp: new Date().toString(),})
         res.status(201).json(departments);
     
     
