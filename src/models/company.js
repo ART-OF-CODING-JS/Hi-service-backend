4e7fb289f61af19e97b589754c9acdf66154f042
Muradazzeh <morad.azza@yahoo.com>
2022-07-31 13:32:57 +0300

    update

--- src/models/company.js
+++ src/models/company.js
@@ -2,33 +2,16 @@
 const company = (sequelize, DataTypes) => sequelize.define('company', {
     companyName: {
       type: DataTypes.STRING,
-      defaultValue: false,
     },
-//     email:{
-//         type: DataTypes.STRING,
-//       //   allowNull: false,
-//   // we can add unique
-    //   },
-    //   password: {
-    //     type: DataTypes.STRING,
-    //   //   allowNu/ll: false,
-    //   }, 
     services: {
       type: DataTypes.ARRAY(DataTypes.STRING),
       defaultValue: [],
     },
     owner: {
       type: DataTypes.STRING,
-      allowNull: false,
-      required: false
+       
     },
-    // phoneNumber: {
-    //   type: DataTypes.INTEGER,
-    //   required: false
-    // },
-//     city: {
-//       type: DataTypes.STRING,
-//   },
+
   location:{
     type: DataTypes.STRING,
   },
