be5a6b0838649fb869435681f8736e4e46476115
Yasein Burqan <mr.yasein@gmail.com>
2022-08-01 12:27:21 +0300

    adding logger

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-'use strict';
+"use strict";
 // require("dotenv").config()
 // Start up DB Server
-const { db } = require('./src/models/index-model');
-db.sync()
-  .then(() => {
-
-    // Start the web server
-    require('./src/server.js').start(process.env.PORT);
-  });
+const { db } = require("./src/models/index-model");
+db.sync().then(() => {
+  // Start the web server
+  require("./src/server.js").start(process.env.PORT);
+});
